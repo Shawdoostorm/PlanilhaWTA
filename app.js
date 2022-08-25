bc762a4ba719723ab5d18a5eefbacb9769ade2e1
Gabriel Marques <101673182+Shawdoostorm@users.noreply.github.com>
2022-08-25 16:50:03 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const TransacoesRepositorio = require("./transacoes-repositorio")
 
 const app = express()
 
-const port = 3000
+const port = 3002
 
 // Permite acessar o req.body
 app.use(express.json());
@@ -14,12 +14,29 @@ app.use(express.static(`${__dirname}/public`))
 app.get('/transacoes', (req, res) => {
     const repositorio = new TransacoesRepositorio()
     const transacoes = repositorio.listarTransacoes()
+    //{
+    //    transacoes;[
+    //        {"valor: "10", "descricao":"bolo", "categoria": "despesa"}
+    //    ]
+    //}
+    let saldo = 0
+    transacoes.transacoes.forEach((transacao)=> {
+        if(transacao.categoria === "Despesa"){
+            saldo = saldo - transacao.valor
+        }
+        if (transacao.categoria === "Receita"){
+            saldo = saldo + transacao.valor
+        }
+    })
+    transacoes.saldo = saldo
+    console.log(transacoes)
     res.send(transacoes)
 })
 
 app.post('/transacoes', (req, res) => {
     const repositorio = new TransacoesRepositorio()
     const transacao = req.body
+    console.log(transacao);
     repositorio.criarTransacao(transacao)
     res.status(201).send(transacao)
 })
